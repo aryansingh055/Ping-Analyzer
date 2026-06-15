ed8ca1b7ee56e186c4c32d8a8f04f83e947a8e35
Aryan Singh <aryansinghas7703@gmail.com>
2026-06-15 23:30:18 +0530

    Update ping-analyzer.sh

--- ping-analyzer.sh
+++ ping-analyzer.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
+
+clear
+
+echo "========================================="
+echo "           PING ANALYZER TOOL"
+echo "========================================="
+
+read -p "Enter Website or IP Address: " website
+
+echo ""
+echo "Checking connectivity..."
+
+current_time=$(date "+%d-%m-%Y %H:%M:%S")
+
+if ping -c 4 "$website" > /dev/null 2>&1
+then
+    status="REACHABLE"
+
+    echo ""
+    echo "Status : $status"
+    echo "Host   : $website"
+    echo "Time   : $current_time"
+
+else
+    status="NOT REACHABLE"
+
+    echo ""
+    echo "Status : $status"
+    echo "Host   : $website"
+    echo "Time   : $current_time"
+fi
+
+echo "$website | $status | $current_time" >> ping_log.txt
+
+echo ""
+echo "Log saved in ping_log.txt"
+echo "========================================="
+
 
